9091b88a4a62c59518e41b9b9011b00d7bebbd57
gozoro <gozoro@yandex.ru>
2024-05-27 20:59:53 +0300

    add base widget class

---
+++ src/BaseWidget.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace gozoro\yii2\widgets;
+
+use yii\base\Widget;
+
+
+/**
+ * Base widget class
+ */
+class BaseWidget extends Widget
+{
+	/**
+	 * Attributes of widget-tag
+	 * @var array
+	 */
+	public $attributes;
+
+	/**
+	 * Jquery-plugin options
+	 * @var array
+	 */
+	public $clientOptions;
+
+	public function init()
+	{
+		parent::init();
+
+		if(\is_null($this->attributes))
+			$this->attributes = [];
+
+
+		if(\is_null($this->clientOptions))
+			$this->clientOptions = [];
+
+
+		if(empty($this->attributes['name']))
+		{
+			$this->attributes['name'] = $this->getId();
+		}
+
+
+		if(empty($this->attributes['id']) and !empty($this->attributes['name']))
+		{
+			$this->attributes['id'] = $this->name2id($this->attributes['name']);
+		}
+
+		if(empty($this->attributes['value']))
+		{
+			$this->attributes['value'] = null;
+		}
+	}
+
+
+	protected function name2id($name)
+	{
+		$id = str_replace('[', '-', $name);
+		$id = str_replace(']', '', $id);
+		return \preg_replace('/\-$/', '', $id);
+	}
+
+	/**
+	 * Returns array with default attribute values
+	 * @return array
+	 */
+	public function getDefaultAttributes()
+	{
+		return [];
+	}
+
+	/**
+	 * Returns array of default client options.
+	 * @return array
+	 */
+	public function getDefaultClientOptions()
+	{
+		return [];
+	}
+
+	/**
+	 * Returs HTML code with widget
+	 * @return string
+	 */
+	public static function widget($config = [])
+    {
+		$widgetConfig = [];
+
+		if(isset($config['clientOptions']))
+		{
+			$widgetConfig['clientOptions'] = $config['clientOptions'];
+			unset($config['clientOptions']);
+		}
+
+		$widgetConfig['attributes'] = $config;
+
+		return parent::widget($widgetConfig);
+	}
+}
